cd82a2823af63348ea7a3adaf62366a2e6f22773
qwe96801 <599189993@qq.com>
2015-12-21 16:27:49 +0800

    测试同步

---
+++ src/com/example/android_git1/changes.java
@@ -0,0 +1,15 @@
+package com.example.android_git1;
+
+public class changes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String str="hi";
+		int a=1;
+		System.out.println(str.toString());
+	}
+
+}
