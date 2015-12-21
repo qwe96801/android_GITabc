dc62647eb05ef9f8a136c3fb4d8b1d2198928ec3
qwe96801 <599189993@qq.com>
2015-12-21 11:07:01 +0800

    添加Service

---
+++ src/com/example/android_git1/Service.java
@@ -0,0 +1,14 @@
+package com.example.android_git1;
+
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Service extends android.app.Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
